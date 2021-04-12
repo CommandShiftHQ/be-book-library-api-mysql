7a36466688bab6d5d9a048801f854b8f22f5ae34
ian-antking <ian.antking@gmail.com>
2021-04-12 19:55:31 +0100

    helpers use async/await

--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -28,80 +28,74 @@ const removePassword = (obj) => {
   return obj;
 };
 
-const getAllItems = (res, model) => {
+const getAllItems = async (res, model) => {
   const Model = getModel(model);
 
   const options = getOptions(model);
 
-  return Model.findAll({ ...options }).then((items) => {
-    const itemsWithoutPassword = items.map((item) =>
-      removePassword(item.dataValues)
-    );
-    res.status(200).json(itemsWithoutPassword);
+  const items = await Model.findAll({...options})
+    
+  const itemsWithoutPassword = items.map((item) => {
+    return removePassword(item.get());
   });
+
+  res.status(200).json(itemsWithoutPassword);
 };
 
-const createItem = (res, model, item) => {
+const createItem = async (res, model, item) => {
   const Model = getModel(model);
 
-  return Model.create(item)
-    .then((newItemCreated) => {
-      const itemWithoutPassword = removePassword(newItemCreated.dataValues);
+  try {
+    const newItem = await Model.create(item);
+    const itemWithoutPassword = removePassword(newItem.get());
+  
+    res.status(201).json(itemWithoutPassword);
+  } catch (error) {
+    const errorMessages = error.errors.map((e) => e.message);
 
-      res.status(201).json(itemWithoutPassword);
-    })
-    .catch((error) => {
-      const errorMessages = error.errors.map((e) => e.message);
-
-      return res.status(400).json({ errors: errorMessages });
-    });
+    res.status(400).json({ errors: errorMessages });
+  }
 };
 
-const updateItem = (res, model, item, id) => {
+const updateItem = async (res, model, item, id) => {
   const Model = getModel(model);
 
-  return Model.update(item, { where: { id } }).then(([recordsUpdated]) => {
-    if (!recordsUpdated) {
-      res.status(404).json(get404Error(model));
-    } else {
-      getModel(model)
-        .findByPk(id)
-        .then((updatedItem) => {
-          const itemWithoutPassword = removePassword(updatedItem.dataValues);
-          res.status(200).json(itemWithoutPassword);
-        });
-    }
-  });
+  const [ itemsUpdated ]= await Model.update(item, { where: { id } });
+
+  if (!itemsUpdated) {
+    res.status(404).json(get404Error(model));
+  } else {
+    const updatedItem = await Model.findByPk(id);
+    const itemWithoutPassword = removePassword(updatedItem.get());
+    res.status(200).json(itemWithoutPassword);
+  }
 };
 
-const getItemById = (res, model, id) => {
+const getItemById = async (res, model, id) => {
   const Model = getModel(model);
 
   const options = getOptions(model);
 
-  return Model.findByPk(id, { ...options }).then((item) => {
-    if (!item) {
-      res.status(404).json(get404Error(model));
-    } else {
-      const itemWithoutPassword = removePassword(item.dataValues);
+  const item = await Model.findByPk(id, { ...options });
 
-      res.status(200).json(itemWithoutPassword);
-    }
-  });
+  if (!item) {
+    res.status(404).json(get404Error(model));
+  } else {
+    const itemWithoutPassword = removePassword(item.dataValues);
+    res.status(200).json(itemWithoutPassword);
+  }
 };
 
-const deleteItem = (res, model, id) => {
+const deleteItem = async (res, model, id) => {
   const Model = getModel(model);
 
-  return Model.findByPk(id).then((foundItem) => {
-    if (!foundItem) {
-      res.status(404).json(get404Error(model));
-    } else {
-      Model.destroy({ where: { id } }).then(() => {
-        res.status(204).send();
-      });
-    }
-  });
+  const itemsDeleted = await Model.destroy({ where: { id } });
+
+  if (!itemsDeleted) {
+    res.status(404).json(get404Error(model));
+  } else {
+    res.status(204).send();
+  }
 };
 
 module.exports = {
